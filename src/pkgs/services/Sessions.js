1710353568b46981ddaa3498eaef8cdd54762886
skywarspro15 <potatobg648@gmail.com>
2026-05-12 01:04:03 +0800

    Encore Sessions boilerplate

---
+++ src/pkgs/services/Sessions.js
@@ -0,0 +1,13 @@
+import { Peer } from "peerjs";
+
+const pkg = {
+  name: "Encore Sessions Service",
+  svcName: "SessionsSvc",
+  type: "svc",
+  privs: 0,
+  start: async function (Root) {},
+  data: {},
+  end: async function () {},
+};
+
+export default pkg;
