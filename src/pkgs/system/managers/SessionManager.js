5ea07cfd0b4b203fb56c0bb30d9f6eada8f3ce03
skywarspro15 <potatobg648@gmail.com>
2026-05-22 02:02:58 +0800

    update Sessions icons

--- src/pkgs/system/managers/SessionManager.js
+++ src/pkgs/system/managers/SessionManager.js
@@ -469,8 +469,10 @@ export default class SessionManager {
         .appendTo(tileContainer);
       new Html("div")
         .classOn("session-tile-icon")
-        .html(
-          `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>`,
+        .append(
+          new Html("ion-icon")
+            .attr({ name: "people" })
+            .styleJs({ width: "50px", height: "50px" }),
         )
         .appendTo(hostTile);
       new Html("div")
@@ -488,8 +490,10 @@ export default class SessionManager {
         .appendTo(tileContainer);
       new Html("div")
         .classOn("session-tile-icon")
-        .html(
-          `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>`,
+        .append(
+          new Html("ion-icon")
+            .attr({ name: "open" })
+            .styleJs({ width: "50px", height: "50px" }),
         )
         .appendTo(joinTile);
       new Html("div")
